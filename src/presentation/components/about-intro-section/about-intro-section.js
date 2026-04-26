3ff8583a0c4cd276d0e9893a310efbac6bcad74b
DaronCameloft <daroncameloftdev@gmail.com>
2026-04-26 11:29:46 -0500

    fix(deploy): deploy fix.

---
+++ src/presentation/components/about-intro-section/about-intro-section.js
@@ -0,0 +1,7 @@
+import { initializeWaveScrollEffect } from '../../../shared/effects/wave-scroll.effect.js';
+import { initializeInfiniteMarqueeEffect } from '../../../shared/effects/infinite-marquee.effect.js';
+
+export function initializeAboutIntroSection() {
+    initializeWaveScrollEffect();
+    initializeInfiniteMarqueeEffect();
+}
