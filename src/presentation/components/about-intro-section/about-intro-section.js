542035ed41e5aafa21d0774cd531afb8204e29bf
Fabrizio Alonso Santi Guerrero <u202411774@upc.edu.pe>
2026-05-15 11:49:57 -0500

    refactor: delete ddd source.

--- src/presentation/components/about-intro-section/about-intro-section.js
+++
@@ -1,7 +0,0 @@
-import { initializeWaveScrollEffect } from '../../../shared/effects/wave-scroll.effect.js';
-import { initializeInfiniteMarqueeEffect } from '../../../shared/effects/infinite-marquee.effect.js';
-
-export function initializeAboutIntroSection() {
-    initializeWaveScrollEffect();
-    initializeInfiniteMarqueeEffect();
-}
